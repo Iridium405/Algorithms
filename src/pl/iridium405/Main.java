8b885ab9e5f03d7c865fdebb399ab9c2bed98d7c
Łukasz Rau <luk.rau.01@gmail.com>
2018-09-18 20:13:48 +0200

    Ascending bubble sort

---
+++ src/pl/iridium405/Main.java
@@ -0,0 +1,14 @@
+package pl.iridium405;
+
+import pl.iridium405.algorithms.BubbleSort;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] tab = new int[] {4, -15, 8, 1, 0, 12, -5, 0, 4, -8, 10, 2, 15};
+
+        BubbleSort sort = new BubbleSort();
+        sort.bubbleSortAsc(tab);
+    }
+}
---
+++ src/pl/iridium405/algorithms/BubbleSort.java
@@ -0,0 +1,25 @@
+package pl.iridium405.algorithms;
+
+public class BubbleSort {
+
+    public void bubbleSortAsc(int[] tab) {
+        int temp;
+        int flag = 1;
+        while (flag > 0) {
+            flag = 0;
+            for (int i = 0; i < tab.length - 1; i++) {
+                if (tab[i] > tab[i + 1]) {
+                    temp = tab[i + 1];
+                    tab[i + 1] = tab[i];
+                    tab[i] = temp;
+                    flag++;
+                }
+            }
+        }
+
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println(tab[i] + " ");
+        }
+    }
+
+}
