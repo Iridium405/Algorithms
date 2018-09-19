86b10026cfc3d7c3c3755546b943314c282d6676
Łukasz Rau <luk.rau.01@gmail.com>
2018-09-19 10:43:12 +0200

    Descending bubble sort added

--- src/pl/iridium405/Main.java
+++ src/pl/iridium405/Main.java
@@ -7,8 +7,12 @@ public class Main {
     public static void main(String[] args) {
 
         int[] tab = new int[] {4, -15, 8, 1, 0, 12, -5, 0, 4, -8, 10, 2, 15};
+        int[] tab2 = new int[] {4, -15, 8, 0, 12, -5, -8, 10, 2, 15};
 
         BubbleSort sort = new BubbleSort();
+
         sort.bubbleSortAsc(tab);
+        System.out.println("----------");
+        sort.bubbleSortDesc(tab2);
     }
 }
--- src/pl/iridium405/algorithms/BubbleSort.java
+++ src/pl/iridium405/algorithms/BubbleSort.java
@@ -5,12 +5,35 @@ public class BubbleSort {
     public void bubbleSortAsc(int[] tab) {
         int temp;
         int flag = 1;
+
         while (flag > 0) {
             flag = 0;
             for (int i = 0; i < tab.length - 1; i++) {
                 if (tab[i] > tab[i + 1]) {
                     temp = tab[i + 1];
                     tab[i + 1] = tab[i];
+                    tab[i] = temp;
+                    flag++;
+                }
+            }
+        }
+
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println(tab[i] + " ");
+        }
+    }
+
+
+    public void bubbleSortDesc(int[] tab) {
+        int temp;
+        int flag = 1;
+
+        while(flag > 0) {
+            flag = 0;
+            for (int i = tab.length-1; i > 0; i--) {
+                if(tab[i] > tab[i-1]) {
+                    temp = tab[i-1];
+                    tab[i-1] = tab[i];
                     tab[i] = temp;
                     flag++;
                 }
